68cbec61f0be66c32c9a1a50534ed62f979918c8
Bhavin1304 <bagulbhavin6@gmail.com>
2025-05-12 16:48:18 +0530

    Add files via upload

---
+++ array 2/script.js
@@ -0,0 +1,52 @@
+// print arry
+
+// let x=[1,2,3,4,false,"hello"]
+
+// for(let i=0; i<x.length;i++){
+//     document.writeln(x[i])
+// }
+
+// print the array
+
+// let nums=[10,50,40,120,300,500,80,90,70]
+
+// for(let i=0; i<nums.length;i++){
+//     document.writeln(nums[i])
+
+// }
+
+// price filter show products which price is more than 100.
+
+// let price=[10,50,40,120,300,500,80,90,70]
+
+// for(let i=0; i<price.length;i++){
+// if(price[i]> 100){
+//     document.writeln(price[i])
+// }
+// }
+
+//Perform addition between two array..
+
+// let a=[1,2,3,4,5]
+// let b=[10,20,30,40,50]
+
+// for(let i=0; i<a.length;i++){
+// document.writeln(a[i]+b[i])
+
+// }
+
+// print sum of array.
+// let cart=[100,450,352,485,695]
+// let total=0
+// for(let i=0;i<cart.length;i++){
+
+//     total = total + cart[i];
+// }
+// document.writeln(total)
+
+
+// printf reverse array.
+// let num=[1,2,3,4,5,6,7,8,9]
+// for(let i=num.length-1;i>=0;i--){
+//     document.writeln(num[i])
+// }
